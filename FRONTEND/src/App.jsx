a94307db39d31f4e667584a33d470e7c62f42db7
Slim-Beatnik <totem64@gmail.com>
2025-10-03 15:20:03 -0700

    Add logo - last push to main, git flow

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import LoadingLogo from './assets/FTS_Logo.jsx'
+import Logo from './logo/Logo'
 import './App.css'
 
 
@@ -9,24 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-          <LoadingLogo variant="dark" className="logo" />
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          <Logo variant={"light"} anim={true} />
+      
     </>
   )
 }
